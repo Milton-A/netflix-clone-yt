a761e869c2ad37ab98189ebfdd67a3d6eb953ab7
Milton-A <miltondantas33@outlook.com>
2025-06-14 19:35:09 +0100

    feat: banner adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ async function Home() {
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={movies.netflixOriginals} />
         <section>
           {/* Row */}
 
