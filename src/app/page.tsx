4f326ff795f066401d3c637b58e1a9705b47bb3a
Milton-A <miltondantas33@outlook.com>
2025-06-14 17:31:04 +0100

    feat: adicionado componente Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Welcome to Netflix Clone</h1>
-      <p className="text-lg">Please login to continue</p>
+    <div className="">
+      <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+
+        </section>
+      </main>
+      {/* Modal */}
     </div>
   );
 }
