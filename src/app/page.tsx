68778f8f2ab8482dcf5928f40fcf894163d2c70f
Milton-A <miltondantas33@outlook.com>
2025-06-15 18:25:20 +0100

    bug: correcao de row scroll controling css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { fetchMovies } from "@/services/movieService";
 async function Home() {
   const movies = await fetchMovies()
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-950/20 to-[#010511] lg:h-[140vh]">
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={movies.netflixOriginals} />
@@ -16,10 +16,10 @@ async function Home() {
           <Row title="Top Rated" movies={movies.topRated} />
           <Row title="Action Movies" movies={movies.actionMovies} />
           {/* My List */}
-          <Row title="Comedy Movies" movies={movies.comedyMovies} />
-          <Row title="Romance Movies" movies={movies.romanceMovies} />
-          <Row title="Horror Movies" movies={movies.horrorMovies} />
-          <Row title="Documentaries Movies " movies={movies.documentaries} />
+          <Row title="Comedy" movies={movies.comedyMovies} />
+          <Row title="Romance" movies={movies.romanceMovies} />
+          <Row title="Horror" movies={movies.horrorMovies} />
+          <Row title="Documentaries" movies={movies.documentaries} />
         </section>
       </main>
       {/* Modal */}
--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -8,7 +8,7 @@ interface ThumbnailProps {
 function Thumbnail({ movie }: ThumbnailProps) {
     return (
         <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
-            <Image src={`${baseUrlW500}${movie.backdrop_path || movie.poster_path}`} alt="" className="rounded-sm object-cover md:rounded" layout="fill" />
+            <Image src={`${baseUrlW500}${movie.backdrop_path || movie.poster_path}`} alt="" className="rounded-sm object-cover md:rounded" fill />
         </div>
     )
 }
