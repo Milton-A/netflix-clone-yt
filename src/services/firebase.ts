040692df5be29d4376755b9c4e179756541b4a89
Milton-A <97224110+Milton-A@users.noreply.github.com>
2025-06-17 10:54:19 +0100

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB85iLVqDvOMMOvSV324uEWKx13Nrr1Vgc",
-    authDomain: "netflix-clone-yt-d6b19.firebaseapp.com",
-    projectId: "netflix-clone-yt-d6b19",
-    storageBucket: "netflix-clone-yt-d6b19.firebasestorage.app",
-    messagingSenderId: "418927956701",
-    appId: "1:418927956701:web:d2f69881872bbd22854134"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET, 
+    messagingSenderId:  process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
